284e8a061c08dfada2110fd312ee977546248bb1
Mikhail Subach <scorpibear@tut.by>
2017-06-07 14:26:14 +0300

    Test for jasmineStarted

--- spec/listener_spec.js
+++ spec/listener_spec.js
@@ -1,8 +1,22 @@
+'use strict';
+
 describe("listener", function() {
   let Listener = require('../jasmine-epam-reportportal-listener');
 
   it('initialized without errors', function() {
     let listener = new Listener({});
     expect(listener.controller).toBeDefined();
+  });
+
+  describe("jasmineStarted", function() {
+    beforeAll(function() {
+      //  define browser if not yet, as used in listener.jasmineStarted
+      global["browser"] = global["browser"] || {getCapabilities: function(){return new Promise(function(){})}};
+    })
+    it("does not throw exceptions", function() {
+      let listener = new Listener({launch: "l1"});
+      listener.jasmineStarted({totalSpecsDefined: 42});
+      expect(listener.totalSpecsToDo).toBe(42);
+    });
   });
 });
