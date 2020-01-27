d46cfbb03ab6650bc44782931747ad0cb9d21336
Katsiaryna Tatarynovich <Katsiaryna_Tatarynovich@epam.com>
2020-01-27 17:37:25 +0300

    EPMRPP-45143: Update jasmine js agent for version5, fix after review

--- examples/protractor/simpleConfig.js
+++ examples/protractor/simpleConfig.js
@@ -1,28 +1,11 @@
 const ReportportalAgent = require('../../lib/reportportal-agent.js');
+const reportportalConfig = require('./reportportalConf');
 let agent;
 
-
 exports.config = {
-
     specs: ['testAngularPage.js', 'testGithubPage.js'],
     onPrepare(){
-            agent = new ReportportalAgent({
-            token: "00000000-0000-0000-0000-000000000000",
-            endpoint: "http://your.reportportal.server/api/v1",
-            launch: "LAUNCH_NAME",
-            project: "PROJECT_NAME",
-            description: 'YOUR_DESCRIPTION',
-            attachPicturesToLogs: false,
-            attributes: [
-                {
-                    "key": "yourKey",
-                    "value": "yourValue"
-                },
-                {
-                    "value": "yourValue"
-                }
-            ]
-        });
+            agent = new ReportportalAgent(reportportalConfig);
 
         jasmine.getEnv().addReporter(agent.getJasmineReporter());
     },
