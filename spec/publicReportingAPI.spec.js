5040eb49b8dc536c844d90c2c7a67fb2533ed659
Bam6ycha <84175555+Bam6ycha@users.noreply.github.com>
2023-05-30 14:19:28 +0500

    EPMRPP 82473 || Fix empty error message (#81)
    * EPMRPP-82473 || Empty error message for test case reported
    
    This commit changes the logic for sending logs to RP.
    Right now, only errors from tests that fail are sent.
    
    * EPMRPP-82473 || Add and configure prettier
    
    * EPMRPP-82473 || Changes after applying prettier
    
    * EPMRPP-82473 || Fix failed tests
    
    * EPMRPP-82473 || Update CHANGELOG.md

--- spec/publicReportingAPI.spec.js
+++ spec/publicReportingAPI.spec.js
@@ -18,181 +18,207 @@ const ClientPublicReportingAPI = require('@reportportal/client-javascript/lib/pu
 const PublicReportingAPI = require('../lib/publicReportingAPI');
 
 const publicReportingAPILaunchLogMethods = [
-    { method: 'launchTrace', level: 'TRACE' },
-    { method: 'launchDebug', level: 'DEBUG' },
-    { method: 'launchInfo', level: 'INFO' },
-    { method: 'launchWarn', level: 'WARN' },
-    { method: 'launchError', level: 'ERROR' },
-    { method: 'launchFatal', level: 'FATAL' },
+  { method: 'launchTrace', level: 'TRACE' },
+  { method: 'launchDebug', level: 'DEBUG' },
+  { method: 'launchInfo', level: 'INFO' },
+  { method: 'launchWarn', level: 'WARN' },
+  { method: 'launchError', level: 'ERROR' },
+  { method: 'launchFatal', level: 'FATAL' },
 ];
 const publicReportingAPILogMethods = [
-    { method: 'trace', level: 'TRACE' },
-    { method: 'debug', level: 'DEBUG' },
-    { method: 'info', level: 'INFO' },
-    { method: 'warn', level: 'WARN' },
-    { method: 'error', level: 'ERROR' },
-    { method: 'fatal', level: 'FATAL' },
+  { method: 'trace', level: 'TRACE' },
+  { method: 'debug', level: 'DEBUG' },
+  { method: 'info', level: 'INFO' },
+  { method: 'warn', level: 'WARN' },
+  { method: 'error', level: 'ERROR' },
+  { method: 'fatal', level: 'FATAL' },
 ];
 const publicReportingAPILaunchStatusMethods = [
-    { method: 'setLaunchStatusPassed', status: 'passed' },
-    { method: 'setLaunchStatusFailed', status: 'failed' },
-    { method: 'setLaunchStatusSkipped', status: 'skipped' },
-    { method: 'setLaunchStatusStopped', status: 'stopped' },
-    { method: 'setLaunchStatusInterrupted', status: 'interrupted' },
-    { method: 'setLaunchStatusCancelled', status: 'cancelled' },
-    { method: 'setLaunchStatusInfo', status: 'info' },
-    { method: 'setLaunchStatusWarn', status: 'warn' },
+  { method: 'setLaunchStatusPassed', status: 'passed' },
+  { method: 'setLaunchStatusFailed', status: 'failed' },
+  { method: 'setLaunchStatusSkipped', status: 'skipped' },
+  { method: 'setLaunchStatusStopped', status: 'stopped' },
+  { method: 'setLaunchStatusInterrupted', status: 'interrupted' },
+  { method: 'setLaunchStatusCancelled', status: 'cancelled' },
+  { method: 'setLaunchStatusInfo', status: 'info' },
+  { method: 'setLaunchStatusWarn', status: 'warn' },
 ];
 const publicReportingAPIStatusMethods = [
-    { method: 'setStatusPassed', status: 'passed' },
-    { method: 'setStatusFailed', status: 'failed' },
-    { method: 'setStatusSkipped', status: 'skipped' },
-    { method: 'setStatusStopped', status: 'stopped' },
-    { method: 'setStatusInterrupted', status: 'interrupted' },
-    { method: 'setStatusCancelled', status: 'cancelled' },
-    { method: 'setStatusInfo', status: 'info' },
-    { method: 'setStatusWarn', status: 'warn' },
+  { method: 'setStatusPassed', status: 'passed' },
+  { method: 'setStatusFailed', status: 'failed' },
+  { method: 'setStatusSkipped', status: 'skipped' },
+  { method: 'setStatusStopped', status: 'stopped' },
+  { method: 'setStatusInterrupted', status: 'interrupted' },
+  { method: 'setStatusCancelled', status: 'cancelled' },
+  { method: 'setStatusInfo', status: 'info' },
+  { method: 'setStatusWarn', status: 'warn' },
 ];
 
 describe('PublicReportingAPI', () => {
-    it('should call clientPublicReportingApi.addAttributes method with attributes and undefined as parameter,'
-        + ' if suite doesn\'t set', () => {
-        spyOn(ClientPublicReportingAPI, 'addAttributes').and.returnValue(() => {});
+  it(
+    'should call clientPublicReportingApi.addAttributes method with attributes and undefined as parameter,' +
+      " if suite doesn't set",
+    () => {
+      spyOn(ClientPublicReportingAPI, 'addAttributes').and.returnValue(() => {});
 
-        PublicReportingAPI.addAttributes([{ key: 'key', value: 'value' }]);
+      PublicReportingAPI.addAttributes([{ key: 'key', value: 'value' }]);
 
-        expect(ClientPublicReportingAPI.addAttributes)
-            .toHaveBeenCalledWith([{ key: 'key', value: 'value' }], undefined);
-    });
+      expect(ClientPublicReportingAPI.addAttributes).toHaveBeenCalledWith(
+        [{ key: 'key', value: 'value' }],
+        undefined
+      );
+    }
+  );
 
-    it('should call clientPublicReportingApi.addAttributes method with attributes and suite as parameter', () => {
-        spyOn(ClientPublicReportingAPI, 'addAttributes').and.returnValue(() => {});
+  it('should call clientPublicReportingApi.addAttributes method with attributes and suite as parameter', () => {
+    spyOn(ClientPublicReportingAPI, 'addAttributes').and.returnValue(() => {});
 
-        PublicReportingAPI.addAttributes([{ key: 'key', value: 'value' }], 'suite');
+    PublicReportingAPI.addAttributes([{ key: 'key', value: 'value' }], 'suite');
 
-        expect(ClientPublicReportingAPI.addAttributes).toHaveBeenCalledWith([{ key: 'key', value: 'value' }], 'suite');
-    });
+    expect(ClientPublicReportingAPI.addAttributes).toHaveBeenCalledWith(
+      [{ key: 'key', value: 'value' }],
+      'suite'
+    );
+  });
 
-    it('should call clientPublicReportingApi.setDescription method with text and undefined as parameters,'
-        + ' if suite doesn\'t set', () => {
-        spyOn(ClientPublicReportingAPI, 'setDescription').and.returnValue(() => {});
+  it(
+    'should call clientPublicReportingApi.setDescription method with text and undefined as parameters,' +
+      " if suite doesn't set",
+    () => {
+      spyOn(ClientPublicReportingAPI, 'setDescription').and.returnValue(() => {});
 
-        PublicReportingAPI.setDescription('text');
+      PublicReportingAPI.setDescription('text');
 
-        expect(ClientPublicReportingAPI.setDescription).toHaveBeenCalledWith('text', undefined);
-    });
+      expect(ClientPublicReportingAPI.setDescription).toHaveBeenCalledWith('text', undefined);
+    }
+  );
 
-    it('should call clientPublicReportingApi.setDescription method with text and suite as parameters', () => {
-        spyOn(ClientPublicReportingAPI, 'setDescription').and.returnValue(() => {});
+  it('should call clientPublicReportingApi.setDescription method with text and suite as parameters', () => {
+    spyOn(ClientPublicReportingAPI, 'setDescription').and.returnValue(() => {});
 
-        PublicReportingAPI.setDescription('text', 'suite');
+    PublicReportingAPI.setDescription('text', 'suite');
 
-        expect(ClientPublicReportingAPI.setDescription).toHaveBeenCalledWith('text', 'suite');
-    });
+    expect(ClientPublicReportingAPI.setDescription).toHaveBeenCalledWith('text', 'suite');
+  });
 
-    it('should call clientPublicReportingApi.setTestCaseId method with testCaseId and undefined as parameters,'
-        + ' if suite doesn\'t set', () => {
-        spyOn(ClientPublicReportingAPI, 'setTestCaseId').and.returnValue(() => {});
+  it(
+    'should call clientPublicReportingApi.setTestCaseId method with testCaseId and undefined as parameters,' +
+      " if suite doesn't set",
+    () => {
+      spyOn(ClientPublicReportingAPI, 'setTestCaseId').and.returnValue(() => {});
 
-        PublicReportingAPI.setTestCaseId('testCaseId');
+      PublicReportingAPI.setTestCaseId('testCaseId');
 
-        expect(ClientPublicReportingAPI.setTestCaseId).toHaveBeenCalledWith('testCaseId', undefined);
-    });
+      expect(ClientPublicReportingAPI.setTestCaseId).toHaveBeenCalledWith('testCaseId', undefined);
+    }
+  );
 
-    it('should call clientPublicReportingApi.setTestCaseId method with testCaseId and suite as parameters', () => {
-        spyOn(ClientPublicReportingAPI, 'setTestCaseId').and.returnValue(() => {});
+  it('should call clientPublicReportingApi.setTestCaseId method with testCaseId and suite as parameters', () => {
+    spyOn(ClientPublicReportingAPI, 'setTestCaseId').and.returnValue(() => {});
 
-        PublicReportingAPI.setTestCaseId('testCaseId', 'suite');
+    PublicReportingAPI.setTestCaseId('testCaseId', 'suite');
 
-        expect(ClientPublicReportingAPI.setTestCaseId).toHaveBeenCalledWith('testCaseId', 'suite');
-    });
+    expect(ClientPublicReportingAPI.setTestCaseId).toHaveBeenCalledWith('testCaseId', 'suite');
+  });
 
-    it('should call clientPublicReportingApi.addLog method with log and suite as parameters', () => {
-        spyOn(ClientPublicReportingAPI, 'addLog').and.returnValue(() => {});
+  it('should call clientPublicReportingApi.addLog method with log and suite as parameters', () => {
+    spyOn(ClientPublicReportingAPI, 'addLog').and.returnValue(() => {});
 
-        PublicReportingAPI.log('INFO', 'message', null, 'suite');
+    PublicReportingAPI.log('INFO', 'message', null, 'suite');
 
-        expect(ClientPublicReportingAPI.addLog)
-            .toHaveBeenCalledWith({ level: 'INFO', file: null, message: 'message' }, 'suite');
-    });
+    expect(ClientPublicReportingAPI.addLog).toHaveBeenCalledWith(
+      { level: 'INFO', file: null, message: 'message' },
+      'suite'
+    );
+  });
 
-    it('should call clientPublicReportingApi.addLog with default parameters if there are no custom ones', () => {
-        spyOn(ClientPublicReportingAPI, 'addLog').and.returnValue(() => {});
+  it('should call clientPublicReportingApi.addLog with default parameters if there are no custom ones', () => {
+    spyOn(ClientPublicReportingAPI, 'addLog').and.returnValue(() => {});
 
-        PublicReportingAPI.log();
+    PublicReportingAPI.log();
 
-        expect(ClientPublicReportingAPI.addLog)
-            .toHaveBeenCalledWith({ level: 'INFO', file: undefined, message: '' }, undefined);
-    });
+    expect(ClientPublicReportingAPI.addLog).toHaveBeenCalledWith(
+      { level: 'INFO', file: undefined, message: '' },
+      undefined
+    );
+  });
 
-    publicReportingAPILogMethods.forEach((item) => {
-        it(`should call clientPublicReportingApi.addLog method with ${item.level}
+  publicReportingAPILogMethods.forEach((item) => {
+    it(`should call clientPublicReportingApi.addLog method with ${item.level}
          level parameter if we run ${item.method} method`, () => {
-            spyOn(ClientPublicReportingAPI, 'addLog').and.returnValue(() => {});
+      spyOn(ClientPublicReportingAPI, 'addLog').and.returnValue(() => {});
 
-            PublicReportingAPI[item.method]('message', null, 'suite');
+      PublicReportingAPI[item.method]('message', null, 'suite');
 
-            expect(ClientPublicReportingAPI.addLog)
-                .toHaveBeenCalledWith({ level: item.level, file: null, message: 'message' }, 'suite');
-        });
+      expect(ClientPublicReportingAPI.addLog).toHaveBeenCalledWith(
+        { level: item.level, file: null, message: 'message' },
+        'suite'
+      );
     });
+  });
 
-    it('should call clientPublicReportingApi.addLaunchLog method with default parameters', () => {
-        spyOn(ClientPublicReportingAPI, 'addLaunchLog').and.returnValue(() => {});
+  it('should call clientPublicReportingApi.addLaunchLog method with default parameters', () => {
+    spyOn(ClientPublicReportingAPI, 'addLaunchLog').and.returnValue(() => {});
 
-        PublicReportingAPI.launchLog();
+    PublicReportingAPI.launchLog();
 
-        expect(ClientPublicReportingAPI.addLaunchLog)
-            .toHaveBeenCalledWith({ level: 'INFO', file: undefined, message: '' });
+    expect(ClientPublicReportingAPI.addLaunchLog).toHaveBeenCalledWith({
+      level: 'INFO',
+      file: undefined,
+      message: '',
     });
+  });
 
-    publicReportingAPILaunchLogMethods.forEach((item) => {
-        it(`should call clientPublicReportingApi.addLaunchLog method with ${item.level}
+  publicReportingAPILaunchLogMethods.forEach((item) => {
+    it(`should call clientPublicReportingApi.addLaunchLog method with ${item.level}
          level parameter if we run ${item.method} method`, () => {
-            spyOn(ClientPublicReportingAPI, 'addLaunchLog').and.returnValue(() => {});
+      spyOn(ClientPublicReportingAPI, 'addLaunchLog').and.returnValue(() => {});
 
-            PublicReportingAPI[item.method]();
+      PublicReportingAPI[item.method]();
 
-            expect(ClientPublicReportingAPI.addLaunchLog)
-                .toHaveBeenCalledWith({ level: item.level, file: undefined, message: '' });
-        });
+      expect(ClientPublicReportingAPI.addLaunchLog).toHaveBeenCalledWith({
+        level: item.level,
+        file: undefined,
+        message: '',
+      });
     });
+  });
 
-    it('should call clientPublicReportingApi.setStatus method', () => {
-        spyOn(ClientPublicReportingAPI, 'setStatus').and.returnValue(() => {});
+  it('should call clientPublicReportingApi.setStatus method', () => {
+    spyOn(ClientPublicReportingAPI, 'setStatus').and.returnValue(() => {});
 
-        PublicReportingAPI.setStatus();
+    PublicReportingAPI.setStatus();
 
-        expect(ClientPublicReportingAPI.setStatus).toHaveBeenCalled();
-    });
+    expect(ClientPublicReportingAPI.setStatus).toHaveBeenCalled();
+  });
 
-    publicReportingAPIStatusMethods.forEach((item) => {
-        it(`should call clientPublicReportingApi.setStatus method with ${item.status}
+  publicReportingAPIStatusMethods.forEach((item) => {
+    it(`should call clientPublicReportingApi.setStatus method with ${item.status}
          status parameter if we run ${item.method} method`, () => {
-            spyOn(ClientPublicReportingAPI, 'setStatus').and.returnValue(() => {});
+      spyOn(ClientPublicReportingAPI, 'setStatus').and.returnValue(() => {});
 
-            PublicReportingAPI[item.method]();
+      PublicReportingAPI[item.method]();
 
-            expect(ClientPublicReportingAPI.setStatus).toHaveBeenCalledWith(item.status, undefined);
-        });
+      expect(ClientPublicReportingAPI.setStatus).toHaveBeenCalledWith(item.status, undefined);
     });
+  });
 
-    it('should call clientPublicReportingApi.setLaunchStatus method', () => {
-        spyOn(ClientPublicReportingAPI, 'setLaunchStatus').and.returnValue(() => {});
+  it('should call clientPublicReportingApi.setLaunchStatus method', () => {
+    spyOn(ClientPublicReportingAPI, 'setLaunchStatus').and.returnValue(() => {});
 
-        PublicReportingAPI.setLaunchStatus();
+    PublicReportingAPI.setLaunchStatus();
 
-        expect(ClientPublicReportingAPI.setLaunchStatus).toHaveBeenCalled();
-    });
+    expect(ClientPublicReportingAPI.setLaunchStatus).toHaveBeenCalled();
+  });
 
-    publicReportingAPILaunchStatusMethods.forEach((item) => {
-        it(`should call clientPublicReportingApi.setStatus method with ${item.status}
+  publicReportingAPILaunchStatusMethods.forEach((item) => {
+    it(`should call clientPublicReportingApi.setStatus method with ${item.status}
          status parameter if we run ${item.method} method`, () => {
-            spyOn(ClientPublicReportingAPI, 'setLaunchStatus').and.returnValue(() => {});
+      spyOn(ClientPublicReportingAPI, 'setLaunchStatus').and.returnValue(() => {});
 
-            PublicReportingAPI[item.method]();
+      PublicReportingAPI[item.method]();
 
-            expect(ClientPublicReportingAPI.setLaunchStatus).toHaveBeenCalledWith(item.status);
-        });
+      expect(ClientPublicReportingAPI.setLaunchStatus).toHaveBeenCalledWith(item.status);
     });
+  });
 });
